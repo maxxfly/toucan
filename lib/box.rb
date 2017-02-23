4e4a30fe81fc16bfd2a1398af94f79d716b94336
JeanMary <jeanmary@gmail.com>
2017-02-23 23:23:02 +0100

    add comment and fix typo

--- lib/box.rb
+++ lib/box.rb
@@ -7,8 +7,11 @@ class Box
     boxes = self.get_all
 
     users.each do |user|
-      user[:available_boxes] = boxes.select do |boxe|
-        (boxe[:contents] & user[:received_contents]).count == 0
+      user[:available_boxes] = boxes.select do |box|
+        # for every box, we keep only the box
+        # where the intersection with the user's available_boxes
+        # gives no element
+        (box[:contents] & user[:received_contents]).count == 0
       end.map{|b| b[:code] }.sort
     end
 
